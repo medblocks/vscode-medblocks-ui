a9a6c20c51eb89c64b529398d0119ff32cea0f16
Karthik Nishanth <karthik.nishanth06@gmail.com>
2021-04-09 19:57:54 +0530

    'snippet'

--- src/extension.ts
+++ src/extension.ts
@@ -3,57 +3,58 @@
 import * as vscode from "vscode";
 
 import { TemplateTreeProvider, TemplateSnippetItem } from "./templateTree";
-import { } from './templateItem'
+import {} from "./templateItem";
 import { getTransform } from "./utils";
-export async function activate(context: vscode.ExtensionContext) {
-	const configuration = await getTransform(vscode.workspace.rootPath);
-	const templateTree = new TemplateTreeProvider(
-		vscode.workspace.rootPath,
-		configuration,
-	);
-	vscode.window.registerTreeDataProvider(
-		"nodeDependencies",
-		templateTree
-	);
-	vscode.commands.registerCommand("nodeDependencies.refreshEntry", () => {
-		templateTree.refreshConfig();
-		templateTree.refresh();
-	});
-	vscode.commands.registerCommand("extension.openPackageOnNpm", (moduleName) =>
-		vscode.commands.executeCommand(
-			"vscode.open",
-			vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)
-		)
-	);
-	vscode.commands.registerCommand(
-		"nodeDependencies.copy",
-		async (node: TemplateSnippetItem) => {
-			if (node.type === 'templateItem') {
-				vscode.env.clipboard.writeText(node.copy(templateTree.transform))
-				vscode.window.showInformationMessage(`Copied ${node.displayLabel}.`);
-			}
-			else {
-				vscode.env.clipboard.writeText(node.copy())
-				vscode.window.showInformationMessage(`Copied snippet ${node.snipppet.name}.`);
-			}
+export function activate(context: vscode.ExtensionContext) {
+  const configuration = getTransform(vscode.workspace.rootPath);
+  const templateTree = new TemplateTreeProvider(
+    vscode.workspace.rootPath,
+    configuration
+  );
+  vscode.window.registerTreeDataProvider("nodeDependencies", templateTree);
+  vscode.commands.registerCommand("nodeDependencies.refreshEntry", () => {
+    templateTree.refreshConfig();
+    templateTree.refresh();
+  });
+  vscode.commands.registerCommand("extension.openPackageOnNpm", (moduleName) =>
+    vscode.commands.executeCommand(
+      "vscode.open",
+      vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)
+    )
+  );
+  vscode.commands.registerCommand(
+    "nodeDependencies.copy",
+    async (node: TemplateSnippetItem) => {
+      if (node.type === "templateItem") {
+        vscode.env.clipboard.writeText(node.copy(templateTree.transform));
+        vscode.window.showInformationMessage(`Copied ${node.displayLabel}.`);
+      } else {
+        vscode.env.clipboard.writeText(node.copy());
+        vscode.window.showInformationMessage(
+          `Copied snippet ${node.snipppet.name}.`
+        );
+      }
+    }
+  );
+  // vscode.commands.registerCommand(
+  // 	"nodeDependencies.deleteEntry",
+  // 	(node: UINode) =>
+  // 		vscode.window.showInformationMessage(
+  // 			`Successfully called delete entry on ${node.label}.`
+  // 		)
+  // );
 
-		}
-	);
-	// vscode.commands.registerCommand(
-	// 	"nodeDependencies.deleteEntry",
-	// 	(node: UINode) =>
-	// 		vscode.window.showInformationMessage(
-	// 			`Successfully called delete entry on ${node.label}.`
-	// 		)
-	// );
+  vscode.workspace.onDidChangeTextDocument(() => {
+    templateTree.setCurrentTextFile(
+      vscode.window.activeTextEditor?.document.getText()
+    );
+    templateTree.refresh();
+  });
 
-	vscode.workspace.onDidChangeTextDocument(() => {
-		templateTree.setCurrentTextFile(vscode.window.activeTextEditor?.document.getText());
-		templateTree.refresh();
-	});
-
-	vscode.window.onDidChangeActiveTextEditor(() => {
-		templateTree.setCurrentTextFile(vscode.window.activeTextEditor?.document.getText());
-		templateTree.refresh();
-	})
+  vscode.window.onDidChangeActiveTextEditor(() => {
+    templateTree.setCurrentTextFile(
+      vscode.window.activeTextEditor?.document.getText()
+    );
+    templateTree.refresh();
+  });
 }
--- src/templateTree.ts
+++ src/templateTree.ts
@@ -1,196 +1,228 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as visit from 'unist-util-visit'
-import { TransformFunction } from './defaultTransform'
-import { pathExists, getTransform } from './utils';
-import { TemplateItem, Snippet, Tree } from './templateItem';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+import * as visit from "unist-util-visit";
+import { TransformFunction } from "./defaultTransform";
+import { pathExists, getTransform } from "./utils";
+import { TemplateItem, Snippet, Tree } from "./templateItem";
 
-export type TemplateSnippetItem = TemplateItem | Snippet
-export class TemplateTreeProvider implements vscode.TreeDataProvider<TemplateSnippetItem> {
+export type TemplateSnippetItem = TemplateItem | Snippet;
+export class TemplateTreeProvider
+  implements vscode.TreeDataProvider<TemplateSnippetItem> {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    TemplateSnippetItem | undefined | void
+  > = new vscode.EventEmitter<TemplateSnippetItem | undefined | void>();
+  readonly onDidChangeTreeData: vscode.Event<
+    TemplateSnippetItem | undefined | void
+  > = this._onDidChangeTreeData.event;
 
-	private _onDidChangeTreeData: vscode.EventEmitter<TemplateSnippetItem | undefined | void> = new vscode.EventEmitter<TemplateSnippetItem | undefined | void>();
-	readonly onDidChangeTreeData: vscode.Event<TemplateSnippetItem | undefined | void> = this._onDidChangeTreeData.event;
+  constructor(
+    public workspaceRoot: string,
+    public transform?: TransformFunction,
+    public textfile: string = vscode.window.activeTextEditor?.document?.getText()
+  ) {}
 
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
 
-	constructor(public workspaceRoot: string, public transform?: TransformFunction, public textfile: string = vscode.window.activeTextEditor?.document?.getText()) {
+  setCurrentTextFile(file: string) {
+    this.textfile = file;
+  }
 
-	}
+  async refreshConfig() {
+    this.transform = await getTransform(this.workspaceRoot);
+  }
 
-	refresh(): void {
-		this._onDidChangeTreeData.fire();
-	}
+  getTreeItem(element: TemplateSnippetItem): vscode.TreeItem {
+    return element.treeItem;
+  }
 
-	setCurrentTextFile(file: string) {
-		this.textfile = file
-	}
+  getChildren(element?: TemplateSnippetItem): Thenable<TemplateSnippetItem[]> {
+    if (!this.workspaceRoot) {
+      return Promise.resolve([]);
+    }
+    if (!element) {
+      return Promise.resolve(this.getTemplates());
+    }
+    if (element.type === "snippet") {
+      throw new Error("Snippet should not have called children");
+    }
+    if (element.leaf) {
+      return Promise.resolve(element.getSnippets(this.transform));
+    } else {
+      return Promise.resolve(element.getChildren());
+    }
 
-	async refreshConfig() {
-		this.transform = await getTransform(this.workspaceRoot)
-	}
+    // return element.data.children.map(child => {
+    // 	let node: UINode
+    // 	if (child.children && child.children.length) {
+    // 		node = new UINode(child.id, child.rmType, child, vscode.TreeItemCollapsibleState.Collapsed)
+    // 	}
+    // 	else {
+    // 		const uiSnippets = this.transform(child)
+    // 		const collapsable = uiSnippets && uiSnippets.length ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
+    // 		node = new UINode(child.id, child.rmType, child, collapsable, uiSnippets)
+    // 		node.contextValue = 'leaf'
+    // 	}
+    // 	const currentFile = vscode.workspace.workspaceFile
 
-	getTreeItem(element: TemplateSnippetItem): vscode.TreeItem {
-		return element.treeItem;
-	}
+    // 	if (currentFile) {
+    // 		const file = vscode.workspace.fs.readFile(currentFile)
+    // 		console.debug(file)
+    // 	}
+    // 	node.iconPath = new vscode.ThemeIcon('circle-outline')
+    // 	return node
+    // })
+  }
 
-	getChildren(element?: TemplateSnippetItem): Thenable<TemplateSnippetItem[]> {
-		if (!this.workspaceRoot) {
-			return Promise.resolve([]);
-		}
-		if (!element) {
-			return Promise.resolve(this.getTemplates())
-		}
-		if (element.type === 'snippet') {
-			throw new Error('Snippet should not have called children')
-		}
-		if (element.leaf) {
-			return Promise.resolve(element.getSnippets(this.transform))
-		} else {
-			return Promise.resolve(element.getChildren())
-		}
+  private getTemplatePaths(): string[] {
+    const templatesPath = path.join(this.workspaceRoot, "templates");
+    if (pathExists(templatesPath)) {
+      const templates = fs.readdirSync(templatesPath);
+      if (!(templates?.length > 0)) {
+        vscode.window.showInformationMessage(
+          `Found no templates in templates directory`
+        );
+      }
+      return templates;
+    } else {
+      vscode.window.showInformationMessage(
+        "Workspace has no templates directory"
+      );
+      return [];
+    }
+  }
 
-		// return element.data.children.map(child => {
-		// 	let node: UINode
-		// 	if (child.children && child.children.length) {
-		// 		node = new UINode(child.id, child.rmType, child, vscode.TreeItemCollapsibleState.Collapsed)
-		// 	}
-		// 	else {
-		// 		const uiSnippets = this.transform(child)
-		// 		const collapsable = uiSnippets && uiSnippets.length ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
-		// 		node = new UINode(child.id, child.rmType, child, collapsable, uiSnippets)
-		// 		node.contextValue = 'leaf'
-		// 	}
-		// 	const currentFile = vscode.workspace.workspaceFile
+  private async getTemplates(): Promise<TemplateItem[]> {
+    const paths = this.getTemplatePaths();
+    const data = await Promise.all(
+      paths.map(async (t) => {
+        const p = path.join(this.workspaceRoot, "templates", t);
+        const string = await fs.promises.readFile(p, "utf-8");
+        try {
+          const json = JSON.parse(string);
+          return new TemplateItem(this.process(json.tree), t);
+        } catch (e) {
+          console.error(e);
+          return;
+        }
+      })
+    );
+    return data.filter((a) => a);
+  }
 
-		// 	if (currentFile) {
-		// 		const file = vscode.workspace.fs.readFile(currentFile)
-		// 		console.debug(file)
-		// 	}
-		// 	node.iconPath = new vscode.ThemeIcon('circle-outline')
-		// 	return node
-		// })
+  private process(tree: Tree): Tree {
+    let paths = [];
+    const preProcess = (element, parent) => {
+      element.path = parent ? `${parent.path}/${element.id}` : element.id;
+      paths = [...paths, element.path];
+      let node: Tree;
+      if (element.children) {
+        node = {
+          ...element,
+          children: element.children.map((child) => preProcess(child, element)),
+        };
+      } else {
+        node = {
+          ...element,
+        };
+      }
+      node = {
+        ...node,
+        status: this.processStatus(node),
+        snippet: this.processSnippets(node),
+      };
+      // console.log(this.processStatus(node))
+      return node;
 
+      // const postProcess = (element) => {
+      // 	// console.log('running postprocess')
+      // 	if (element.children) {
+      // 		element.children.forEach(postProcess)
+      // 	}
+      // 	console.log('postprocess: ', element.path)
+      // }
+    };
+    const preprocessed = preProcess(tree, null);
+    return preprocessed;
+  }
 
+  private processSnippets(tree: Tree): string {
+    const leaf = !tree?.children?.length;
+    const mandatory = tree.min > 1;
+    const present = this.pathPresent(tree.path);
 
-	}
+    if (leaf) {
+      return tree.snippet;
+    } else {
+      return tree.children
+        .map((c) => {
+          if (c.children) {
+            c.children.map((t) => {
+              if (t.children) {
+                t.children.map((s) => {
+                  console.debug(s);
+                });
+              }
+            });
+          }
+        })
+        .join();
+    }
+    return "";
+  }
 
-	private getTemplatePaths(): string[] {
-		const templatesPath = path.join(this.workspaceRoot, 'templates');
-		if (pathExists(templatesPath)) {
-			const templates = fs.readdirSync(templatesPath)
-			if (!(templates?.length > 0)) {
-				vscode.window.showInformationMessage(`Found no templates in templates directory`);
-			}
-			return templates
-		} else {
-			vscode.window.showInformationMessage('Workspace has no templates directory');
-			return [];
-		}
-	}
+  private processStatus(
+    tree: Tree
+  ): "present" | "optionalAbsent" | "mandatoryAbsent" | "allPresent" {
+    const leaf = !tree?.children?.length;
+    const mandatory = tree.min > 1;
+    const present = this.pathPresent(tree.path);
 
-	private async getTemplates(): Promise<TemplateItem[]> {
-		const paths = this.getTemplatePaths()
-		const data = await Promise.all(paths
-			.map(async (t) => {
-				const p = path.join(this.workspaceRoot, 'templates', t)
-				const string = await fs.promises.readFile(p, 'utf-8')
-				try {
-					const json = JSON.parse(string)
-					return new TemplateItem(this.process(json.tree), t)
-				}
-				catch (e) {
-					console.error(e)
-					return
-				}
-			}))
-		return data.filter(a => a)
-	}
+    if (leaf && present) {
+      return "allPresent";
+    }
 
+    if (!present) {
+      return mandatory ? "mandatoryAbsent" : "optionalAbsent";
+    }
+    // Checks for group
+    const someChildrenNotPresent = tree.children.some(
+      (child) => child.status !== "allPresent"
+    );
+    if (mandatory) {
+      if (someChildrenNotPresent) {
+        if (tree.children.some((child) => child.status === "mandatoryAbsent")) {
+          return "mandatoryAbsent";
+        } else {
+          return "present";
+        }
+      } else {
+        return "allPresent";
+      }
+    } else {
+      if (someChildrenNotPresent) {
+        if (tree.children.some((child) => child.status === "mandatoryAbsent")) {
+          if (
+            tree.children.some((child) => child.status !== "optionalAbsent")
+          ) {
+            return "mandatoryAbsent";
+          } else {
+            return "optionalAbsent";
+          }
+        } else {
+          return "present";
+        }
+      } else {
+        return "allPresent";
+      }
+    }
+  }
 
-	private process(tree: Tree): Tree {
-		let paths = []
-		const preProcess = (element, parent) => {
-			element.path = parent ? `${parent.path}/${element.id}` : element.id
-			paths = [...paths, element.path]
-			let node: Tree
-			if (element.children) {
-				node = {
-					...element,
-					children: element.children.map(child => preProcess(child, element))
-				}
-			} else {
-				node = {
-					...element,
-				}
-			}
-			node = { ...node, status: this.processStatus(node) }
-			// console.log(this.processStatus(node))
-			return node
-
-			// const postProcess = (element) => {
-			// 	// console.log('running postprocess')
-			// 	if (element.children) {
-			// 		element.children.forEach(postProcess)
-			// 	}
-			// 	console.log('postprocess: ', element.path)
-			// }
-		}
-		const preprocessed = preProcess(tree, null)
-		return preprocessed
-	}
-
-	private processSnippets(tree: Tree): string {
-		return ''
-	}
-
-	private processStatus(tree: Tree): 'present' | 'optionalAbsent' | 'mandatoryAbsent' | 'allPresent' {
-		const leaf = !tree?.children?.length
-		const mandatory = tree.min > 1
-		const present = this.pathPresent(tree.path)
-
-
-		if (leaf && present) {
-			return 'allPresent'
-		}
-
-		if (!present) {
-			return mandatory ? 'mandatoryAbsent' : 'optionalAbsent'
-		}
-		// Checks for group
-		const someChildrenNotPresent = tree.children.some(child => child.status !== 'allPresent')
-		if (mandatory) {
-			if (someChildrenNotPresent) {
-				if (tree.children.some(child => child.status === 'mandatoryAbsent')) {
-					return 'mandatoryAbsent'
-				} else {
-					return 'present'
-				}
-			} else {
-				return 'allPresent'
-			}
-		} else {
-			if (someChildrenNotPresent) {
-				if (tree.children.some(child => child.status === 'mandatoryAbsent')) {
-					if (tree.children.some(child => child.status !== 'optionalAbsent')) {
-						return 'mandatoryAbsent'
-					} else {
-						return 'optionalAbsent'
-					}
-				} else {
-					return 'present'
-				}
-			} else {
-				return 'allPresent'
-			}
-		}
-	}
-
-	private pathPresent(path: string): boolean {
-		const result = this.textfile.search(path) >= 0
-		return result
-		// return false
-	}
-
-
-
+  private pathPresent(path: string): boolean {
+    const result = this.textfile.search(path) >= 0;
+    return result;
+    // return false
+  }
 }
