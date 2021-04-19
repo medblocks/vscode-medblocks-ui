541289899b99db2823bb864969f7c1b57529a959
Karthik Nishanth <karthik.nishanth06@gmail.com>
2021-04-19 19:10:22 +0530

    Merge branch 'master' of https://github.com/sidharthramesh/vscode-medblocks-ui into karthik

--- src/templateItem.ts
+++ src/templateItem.ts
@@ -1,128 +1,142 @@
 import { ThemeIcon, TreeItem, TreeItemCollapsibleState } from "vscode";
 
 export interface Tree {
-	id: string,
-	name: string,
-	min: number,
-	max: number,
-	aqlPath: string,
-	rmType: string,
-	localizedName?: string,
-	nodeID?: string,
-	inContext?: boolean,
-	localizedNames?: {
-		[key: string]: string
-	}
-	localizedDescriptions?: {
-		[key: string]: string
-	},
-	annotations?: {
-		[key: string]: string
-	}
-	children?: Tree[],
+  id: string;
+  name: string;
+  min: number;
+  max: number;
+  aqlPath: string;
+  rmType: string;
+  localizedName?: string;
+  nodeID?: string;
+  inContext?: boolean;
+  localizedNames?: {
+    [key: string]: string;
+  };
+  localizedDescriptions?: {
+    [key: string]: string;
+  };
+  annotations?: {
+    [key: string]: string;
+  };
+  children?: Tree[];
 
-	[other: string]: any
-	// Added
-	path?: string
-	runtimeRegex?: string
-	regex?: string
-	snippet?: string,
-	status?: 'present' | 'optionalAbsent' | 'mandatoryAbsent' | 'allPresent',
+  [other: string]: any;
+  // Added
+  path?: string;
+  runtimeRegex?: string;
+  regex?: string;
+  snippet?: string;
+  status?: "present" | "optionalAbsent" | "mandatoryAbsent" | "allPresent";
 }
 
 export class TemplateItem {
-	type: 'templateItem' = 'templateItem'
+  type: "templateItem" = "templateItem";
 
-	iconMap = {
-		present: 'check',
-		optionalAbsent: 'debug-breakpoint-unverified',
-		mandatoryAbsent: 'issues',
-		allPresent: 'check-all'
-	}
+  iconMap = {
+    present: "check",
+    optionalAbsent: "debug-breakpoint-unverified",
+    mandatoryAbsent: "issues",
+    allPresent: "check-all",
+  };
 
+  constructor(public tree: Tree, public label?: string) {}
 
-	constructor(public tree: Tree, public label?: string) { }
+  get icon(): ThemeIcon {
+    return new ThemeIcon(this.iconMap[this.tree.status]);
+  }
 
-	get icon(): ThemeIcon {
-		return new ThemeIcon(this.iconMap[this.tree.status])
-	}
+  get displayLabel() {
+    return this.label || this.tree.name || this.tree.id;
+  }
 
-	get displayLabel() {
-		return this.label || this.tree.name || this.tree.id
-	}
+  get treeItem(): TreeItem {
+    const item = new TreeItem(this.displayLabel, this.collapsibleState);
+    item.tooltip = this.tooltip;
+    item.description = this.tree.rmType.toLocaleLowerCase();
+    item.iconPath = this.icon;
+    item.contextValue = "leaf";
+    return item;
+  }
 
-	get treeItem(): TreeItem {
-		const item = new TreeItem(this.displayLabel, this.collapsibleState)
-		item.tooltip = this.tooltip
-		item.description = this.tree.rmType.toLocaleLowerCase()
-		item.iconPath = this.icon
-		item.contextValue = 'leaf'
-		return item
-	}
-
-	get tooltip(): string {
-		const exclude = ['children', 'inputs', 'snippet', 'localizedName', 'localizedDescriptions']
-		const tree = { ...this.tree, description: this.tree?.localizedDescriptions?.['en'] }
-		const statusDescriptions = {
-			'allPresent': "All child elements are present",
-			'optionalAbsent': "The element is optional, it's currently absent in this document",
-			'mandatoryAbsent': "This element (or some children) is mandatory, but is absent in the current document",
-			'present': "All mandatory child elements are present"
-		}
-		const str = `${tree.name || tree.id} | ${tree.rmType}
-${tree.min}..${tree.max === -1 ? '*' : tree.max}
+  get tooltip(): string {
+    const exclude = [
+      "children",
+      "inputs",
+      "snippet",
+      "localizedName",
+      "localizedDescriptions",
+    ];
+    const tree = {
+      ...this.tree,
+      description: this.tree?.localizedDescriptions?.["en"],
+    };
+    const statusDescriptions = {
+      allPresent: "All child elements are present",
+      optionalAbsent:
+        "The element is optional, it's currently absent in this document",
+      mandatoryAbsent:
+        "This element (or some children) is mandatory, but is absent in the current document",
+      present: "All mandatory child elements are present",
+    };
+    const str = `${tree.name || tree.id} | ${tree.rmType}
+${tree.min}..${tree.max === -1 ? "*" : tree.max}
 ${statusDescriptions[tree.status]}
 Path: ${tree.path}
-${tree.description ? `Description: ${tree.description}\n` : ''}${tree.aqlPath ? `AQL Path: ${tree.aqlPath}\n` : ''}`
-		return str
-	}
+${tree.description ? `Description: ${tree.description}\n` : ""}${
+      tree.aqlPath ? `AQL Path: ${tree.aqlPath}\n` : ""
+    }`;
+    return str;
+  }
 
-	get collapsibleState() {
-		return TreeItemCollapsibleState.Collapsed
-	}
+  get collapsibleState() {
+    return TreeItemCollapsibleState.Collapsed;
+  }
 
-	get leaf(): boolean {
-		return this.tree.children && this.tree.children.length > 0 ? false : true
-	}
+  get leaf(): boolean {
+    return this.tree.children && this.tree.children.length > 0 ? false : true;
+  }
 
-	get mandatory(): boolean {
-		return this.tree.min >= 1
-	}
+  get mandatory(): boolean {
+    return this.tree.min >= 1;
+  }
 
-	getChildren(): TemplateItem[] {
-		return this.tree.children.map(child => new TemplateItem(child))
-	}
+  getChildren(): TemplateItem[] {
+    return this.tree.children.map((child) => new TemplateItem(child));
+  }
 
-	copy(): string {
-		return this.tree.snippet
-	}
+  copy(): string {
+    return this.tree.snippet;
+  }
 
-	getSnippets(transform: Function): Snippet[] {
-		if (!this.leaf) {
-			throw new Error('cannot get snippets on a non leaf node')
-		}
-		const snippets = transform(this.tree)
-		return snippets.map(s => new Snippet(s))
-	}
+  getSnippets(transform: Function): Snippet[] {
+    if (!this.leaf) {
+      throw new Error("cannot get snippets on a non leaf node");
+    }
+    const snippets = transform(this.tree);
+    return snippets.map((s) => new Snippet(s));
+  }
 
-	getRegex(): string {
-		return new RegExp(this.tree.regex).toString()
-	}
-
+  getRegex(): string {
+    return new RegExp(this.tree.regex).toString();
+  }
 }
 
 export class Snippet {
-	type: 'snippet' = 'snippet'
-	constructor(public snipppet: { html: string, name: string }) { }
+  type: "snippet" = "snippet";
+  constructor(public snipppet: { html: string; name: string }) {}
 
-	get treeItem(): TreeItem {
-		const item = new TreeItem(this.snipppet.name, TreeItemCollapsibleState.None)
-		item.contextValue = 'snippet'
-		item.iconPath = new ThemeIcon('code')
-		return item
-	}
+  get treeItem(): TreeItem {
+    const item = new TreeItem(
+      this.snipppet.name,
+      TreeItemCollapsibleState.None
+    );
+    item.contextValue = "snippet";
+    item.iconPath = new ThemeIcon("code");
+    return item;
+  }
 
-	copy() {
-		return this.snipppet.html
-	}
+  copy() {
+    return this.snipppet.html;
+  }
 }
