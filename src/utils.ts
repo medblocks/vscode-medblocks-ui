6d1d503b8bcaa46fcb247c5b9ce846da0235f2ed
neutralboy <leopoorna99@gmail.com>
2022-08-15 22:43:16 +0530

    process only json files in template folder

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as path from "path";
 import {transform} from "medblocks-ui/dist/utils";
 import type { TransformFunction } from "medblocks-ui/dist/utils";
 
@@ -15,3 +16,8 @@ export function pathExists(p: string): boolean {
 export function getTransform(workspace: string): TransformFunction {
   return transform;
 }
+
+export function getExtension(filename: string){
+  const extension = path.extname(filename||'').split('.');
+  return extension[extension.length - 1];
+};
